ef7a7bd13cb670db749ec98c15c4763cd0ee3593
Casper <cms.vandenberg@gmail.com>
2019-06-17 13:54:57 +0200

    added textures and the project

---
+++ Wardrobe/Resources/Database/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MySql.Data.MySqlClient;
+
+namespace Wardrobe.Resources.Database
+{
+    public class DbConnection : IDisposable
+    {
+        public MySqlConnection Connection { get; protected set; }
+        public DbConnection()
+        {
+            Connection = new MySqlConnection("server = localhost; user = root; database = stendotchi; port = 3306; password =");
+            Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            Connection.Close();
+        }
+    }
+}
---
+++ Wardrobe/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "5b0d78c3-4cd6-40f9-ab73-76714a6a5f4a",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
