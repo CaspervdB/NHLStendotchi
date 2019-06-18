f35db01d75b2371c80f74d12e94d6d47ff15f7ed
Casper <cms.vandenberg@gmail.com>
2019-06-18 12:31:57 +0200

    IDK

--- Wardrobe/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Wardrobe/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "6aa02dd0-69ef-45a7-a5cf-4795d4f2e4d9",
+		"XAMARIN_BUILD_ID", "1496d85c-fff5-4599-af0c-5350c97308d0",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
