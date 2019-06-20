756bb38a7a5a4a0b6e5dc646f46e8710788676cc
Casper <casper.van.den.berg@student.nhlstenden.com>
2019-06-20 16:12:57 +0200

    merge conflikt

--- Wardrobe/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ Wardrobe/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,11 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
+<<<<<<< Updated upstream
 		"XAMARIN_BUILD_ID", "70fc218c-2b1f-43db-81b2-b0465ad574ba",
+=======
+		"XAMARIN_BUILD_ID", "81db8724-25d5-4670-9fbc-1f2826c1b2f4",
+>>>>>>> Stashed changes
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
