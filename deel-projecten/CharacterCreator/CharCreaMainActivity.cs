8f20aae29968485a7d84c3ec273b847cdc6fc8c8
Casper <casper.van.den.berg@student.nhlstenden.com>
2019-06-20 14:55:08 +0200

    Wardrobe 0.8

---
+++ deel-projecten/CharacterCreator/CharCreaMainActivity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Web;
+using Android.Systems;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CharacterCreator
+{
+    [Activity(Label = "CharacterCreator", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    public class CharCreaMainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.CharCrea_activity_main);
+
+            var charCreatorHairButtTerug = FindViewById<ImageButton>(Resource.Id.charCreatorHairButtTerug);
+
+        }
+
+        private object FindViewById<T>(object charCreatorHairButtTerug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
